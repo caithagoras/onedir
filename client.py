3abc78d9ab51a6e82651367564381fbcfe695a0d
joubini <joubini@gmail.com>
2013-10-27 17:55:26 -0400

    added user input for username/password

--- client.py
+++ client.py
@@ -5,11 +5,16 @@ from twisted.internet.endpoints import TCP4ClientEndpoint, connectProtocol
 class SendMsg(Protocol):
     
     def connectionMade(self):
+        option = raw_input("Enter 'n' for new user or 'u' for existing user: ")
+        if option == 'n':
+            print "Creating new user!"
+        elif option == 'u':
+            print "Enter your credentials-"
         user = raw_input("Username: ")
         pword = raw_input("Password: ")
         reactor.callLater(2,self.sendMessage, "validate\t" + user + "\t" + pword)
         #reactor.callLater(2,self.sendMessage, "validate\tTom\tpassword123")
-        print ("Connection made.")
+        print "Connection made."
         
     def sendMessage(self, msg):
         self.transport.write(msg)
@@ -24,3 +29,5 @@ class SendMsg(Protocol):
 point = TCP4ClientEndpoint(reactor, "localhost", 3240)
 d = connectProtocol(point, SendMsg())
 reactor.run()
+
+#things to add: check if userinfo.txt exists, create if it doesnt. read in information and attempt auto log in
