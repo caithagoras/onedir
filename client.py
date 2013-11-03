dc06dbf722611200ec25bedd81c541666f236a81
joubini <joubini@gmail.com>
2013-11-03 17:31:58 -0500

    added password/user remembering

--- client.py
+++ client.py
@@ -1,20 +1,41 @@
 from twisted.internet import reactor
 from twisted.internet.protocol import Protocol
 from twisted.internet.endpoints import TCP4ClientEndpoint, connectProtocol
+import os.path
+
 
 class SendMsg(Protocol):
-    
+
+    user = ""
+    password = ""
+
     def connectionMade(self):
-        option = raw_input("Enter 'n' for new user or 'u' for existing user: ")
-        if option == 'n':
-            print "Creating new user!"
-        elif option == 'u':
-            print "Enter your credentials-"
-        user = raw_input("Username: ")
-        pword = raw_input("Password: ")
-        reactor.callLater(2,self.sendMessage, "validate\t" + user + "\t" + pword)
-        #reactor.callLater(2,self.sendMessage, "validate\tTom\tpassword123")
-        print "Connection made."
+        global user
+        global password
+        if os.path.isfile("Login.txt"):            # if login info has been saved, see if they want to use it
+            useCredentials = raw_input("Do you want to use your saved information? Enter 'y' for yes or 'n' for no: ")
+            if useCredentials == 'y':
+                myFile = open("Login.txt", 'rb')
+                credentials = myFile.read()
+                #print credentials
+                pieces = credentials.split("\t")
+                hide = ""
+                for index in range(len(pieces[3])):
+                    hide += "*"
+                print "Username: " + pieces[2] + "\nPassword: " + hide
+                reactor.callLater(2, self.sendMessage, credentials)
+            elif useCredentials == 'n':
+                os.remove("Login.txt")
+                option = raw_input("Enter 'n' for new user or 'u' for existing user: ")
+                if option == 'n':
+                    print "Creating new user!"
+                elif option == 'u':
+                    print "Enter your credentials!"
+                user = raw_input("Username: ")
+                password = raw_input("Password: ")
+                reactor.callLater(2, self.sendMessage, "validate\t" + option + "\t" + user + "\t" + password)
+                #reactor.callLater(2,self.sendMessage, "validate\tTom\tpassword123")
+                print "Connection made."
         
     def sendMessage(self, msg):
         self.transport.write(msg)
@@ -22,12 +43,20 @@ class SendMsg(Protocol):
         
     def dataReceived(self, data):
         if data == "Login_accepted":
-            print("Login attempt succeeded. You are logged in.")
+            print "Login attempt succeeded. You are logged in."
+            if not os.path.isfile("Login.txt"):
+                auto = raw_input("Would you like to automatically log in next time? Enter 'y' for yes or 'n' for no: ")
+                if auto == 'y':
+                    self.logIn()
+                print "Information saved."
         elif data == "Login_rejected":
             print("Login attempt failed. You are not logged in.")
+
+    def logIn(self):
+        myFile = open("Login.txt", 'wb')
+        myFile.write("validate\t" + 'u' + "\t" + user + "\t" + password)
+        myFile.close()
 
 point = TCP4ClientEndpoint(reactor, "localhost", 3240)
 d = connectProtocol(point, SendMsg())
 reactor.run()
-
-#things to add: check if userinfo.txt exists, create if it doesnt. read in information and attempt auto log in
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from twisted.internet.protocol import Protocol, Factory
 from twisted.protocols.basic import LineReceiver
 from twisted.internet import reactor
 
+
 database = {"Tom": "password123",
             "Joe": "thisIsPassword",
             "Jim": "123456abcd"}
@@ -19,18 +20,22 @@ class UserConnection(Protocol):
         pieces = data.split("\t")
         msgType = pieces[0]
 
-        if (msgType == "validate"):
-            user = pieces[1]
-            password = pieces[2]
-            print("Login attempt:\nUser:\t\t" + user + "\nPassword:\t" + password)
+        if msgType == "validate":
+            option = pieces[1]
+            user = pieces[2]
+            password = pieces[3]
+            print "Login attempt:\nUser:\t\t" + user + "\nPassword:\t" + password
+            if option == 'n':
+                print "Creating New User..."
+                database[user] = password
             if database.has_key(user) and database[user] == password:
                 authenticated = True
                 username = user
                 self.transport.write("Login_accepted");
-                print("Accepted")
+                print "Accepted"
             else:
                 self.transport.write("Login_rejected")
-                print("Rejected")
+                print "Rejected"
         
         
 
